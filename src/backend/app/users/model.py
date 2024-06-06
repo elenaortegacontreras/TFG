dd154ae7c15c9affe0f8f30f170d66f738c39d82
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-06-07 00:52:07 +0200

    Add volume to make it persistent

--- src/backend/app/users/model.py
+++ src/backend/app/users/model.py
@@ -6,6 +6,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String, nullable=False)
-    phone = Column(String, unique=True, nullable=True)
+    phone = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
