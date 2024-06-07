7e23d43ef1d7028a377fd43807ba1002202bff9f
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-06-07 03:57:54 +0200

    Add DB tables creation

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -7,7 +7,28 @@ from app.users import model as user_model
 from app.users.model import User
 from app.users.schema import UserRequest, UserResponse
 
+from app.categories import model as category_model
+from app.categories.model import Category
+from app.categories.schema import CategoryRequest, CategoryResponse
+
+from app.savings import model as saving_model
+from app.savings.model import Saving
+from app.savings.schema import SavingRequest, SavingResponse
+
+from app.budgets import model as budget_model
+from app.budgets.model import Budget
+from app.budgets.schema import BudgetRequest, BudgetResponse
+
+from app.transactions import model as transaction_model
+from app.transactions.model import Transaction
+from app.transactions.schema import TransactionRequest, TransactionResponse
+
 user_model.Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
+category_model.Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
+saving_model.Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
+budget_model.Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
+transaction_model.Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
+
 
 app = FastAPI()
 
