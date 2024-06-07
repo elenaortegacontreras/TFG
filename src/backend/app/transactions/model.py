2615bc61a704c982a43312c6384258fb6eaf5974
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-06-07 02:55:39 +0200

    Add transactions table

---
+++ src/backend/app/transactions/model.py
@@ -0,0 +1,13 @@
+from app.db.database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DECIMAL, Date, TIMESTAMP
+
+class Transaction(Base):
+    __tablename__ = 'transaction'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    amount = Column(DECIMAL(15,2), nullable=False)
+    transaction_type = Column(String, nullable=False) # expense or income
+    description = Column(String)
+    insert_date = Column(TIMESTAMP, default=datetime.datetime.utcnow)
---
+++ src/backend/app/transactions/schema.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+class TransactionBase(BaseModel):
+    user_id: int
+    category_id: int
+    name: str
+    amount: float
+    transaction_type: str
+    description: str = None
+
+    class Config:
+        orm_mode = True
+        
+class TransactionRequest(TransactionBase):
+    class Config:
+        orm_mode = True
+
+class TransactionResponse(TransactionBase):
+    id: int
+    insert_date: str
+
+    class Config:
+        orm_mode = True
