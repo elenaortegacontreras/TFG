757375b94f0e816aef8dbd37ead8414b595454da
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-06-23 14:15:47 +0200

    Move database file

---
+++ src/backend/app/core/models/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+hostname = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT")
+database = os.getenv("POSTGRES_DB")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{hostname}:{port}/{database}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# Crear una sesión de base de datos
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
