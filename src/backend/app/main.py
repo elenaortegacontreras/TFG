ec807718be76c15d1059918f7b6e4799429ba33c
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-06-23 14:18:54 +0200

    Update imports after move schemas and models to single files

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,38 +1,16 @@
 from fastapi import FastAPI, status, Depends
 from sqlalchemy.orm import Session
-from app.db.database import engine, get_db, Base
+from app.core.models.database import engine, get_db, Base
 from typing import List
 
-from app.users import model as user_model
-from app.users.model import User
-from app.users.schema import UserRequest, UserResponse
-
-from app.categories import model as category_model
-from app.categories.model import Category
-from app.categories.schema import CategoryRequest, CategoryResponse
-
-from app.subcategories import model as subcategory_model
-from app.subcategories.model import Subcategory
-from app.subcategories.schema import SubcategoryRequest, SubcategoryResponse
-
-from app.savings_goals import model as goal_model
-from app.savings_goals.model import Goal
-from app.savings_goals.schema import GoalRequest, GoalResponse
-
-from app.transactions import model as transaction_model
-from app.transactions.model import Transaction
-from app.transactions.schema import TransactionRequest, TransactionResponse
-
-from app.shops import model as shop_model
-from app.shops.model import Shop
-from app.shops.schema import ShopRequest, ShopResponse
+from app.core.models.models import User, Category, Subcategory, Goal, Transaction, Shop
+from app.core.schemas.schemas import UserRequest, UserResponse, CategoryRequest, CategoryResponse, SubcategoryRequest, SubcategoryResponse, GoalRequest, GoalResponse, TransactionRequest, TransactionResponse, ShopRequest, ShopResponse
 
 # Shop.__table__.drop(bind=engine, checkfirst=True)
-
 # Base.metadata.drop_all(bind=engine, checkfirst=True) # Borrar la tabla en la base de datos
-Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
-
 # user_model.Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
+
+Base.metadata.create_all(bind=engine) # Crear la tabla en la base de datos
 
 app = FastAPI()
 
@@ -103,7 +81,7 @@ def get_all_transactions(db: Session = Depends(get_db)):
     return all_transactions
 
 @app.post("/transactions", status_code=status.HTTP_201_CREATED, response_model=TransactionResponse)
-def create_income(transaction: TransactionRequest, db: Session = Depends(get_db)):
+def create_transaction(transaction: TransactionRequest, db: Session = Depends(get_db)):
     new_income = Transaction(**transaction.dict())
     db.add(new_income)
     db.commit()
