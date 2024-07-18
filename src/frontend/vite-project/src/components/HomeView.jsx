829a8187dbc133a29ab4b4b83f01861f6fac9148
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-07-18 22:22:12 +0200

    Add initial version of homeview component (API interaction)

--- src/frontend/vite-project/src/components/HomeView.jsx
+++ src/frontend/vite-project/src/components/HomeView.jsx
@@ -1,8 +1,11 @@
 import { ResumeTitle } from './ResumeTitle.jsx'
+import { PieChart } from './PieChart.jsx';
+
 import { useNavigate } from 'react-router-dom';
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
 
 export function HomeView() {
     
@@ -10,6 +13,12 @@ export function HomeView() {
     const [savings_amount, setSavingsAmount] = useState(null);
     const [incomes_amount, setIncomesAmount] = useState(null);
     const [amount, setAmount] = useState(null);
+    const [cashExpenses, setCashExpenses] = useState(0);
+    const [cardExpenses, setCardExpenses] = useState(0);
+    const [cashIncomes, setCashIncomes] = useState(0);
+    const [cardIncomes, setCardIncomes] = useState(0);
+    const [cashSavings, setCashSavings] = useState(0);
+    const [cardSavings, setCardSavings] = useState(0);
     const [cash, setCash] = useState(0);
     const [card, setCard] = useState(0);
 
@@ -19,8 +28,8 @@ export function HomeView() {
         axios.get('http://localhost:8000/total_expenses')
             .then(response => {
                 setExpensesAmount(response.data.amount);
-                setCash(cash + response.data.cash);
-                setCard(card + response.data.card);
+                setCashExpenses(response.data.cash);
+                setCardExpenses(response.data.card);
             })
             .catch(error => {
                 console.error('Error fetching the expenses:', error);
@@ -31,8 +40,8 @@ export function HomeView() {
         axios.get('http://localhost:8000/total_savings')
             .then(response => {
                 setSavingsAmount(response.data.amount);
-                setCash(cash + response.data.cash);
-                setCard(card + response.data.card);
+                setCashSavings(response.data.cash);
+                setCardSavings(response.data.card);
             })
             .catch(error => {
                 console.error('Error fetching the savings:', error);
@@ -43,8 +52,8 @@ export function HomeView() {
         axios.get('http://localhost:8000/total_incomes')
             .then(response => {
                 setIncomesAmount(response.data.amount);
-                setCash(cash + response.data.cash);
-                setCard(card + response.data.card);
+                setCashIncomes(response.data.cash);
+                setCardIncomes(response.data.card);
             })
             .catch(error => {
                 console.error('Error fetching the incomes:', error);
@@ -53,19 +62,24 @@ export function HomeView() {
 
     useEffect(() => {
         if (expenses_amount !== null && savings_amount !== null && incomes_amount !== null) {
-            setAmount(expenses_amount - savings_amount - incomes_amount);
+            setAmount(incomes_amount - savings_amount - expenses_amount);
+            setCash(cashIncomes - cashSavings - cashExpenses);
+            setCard(cardIncomes - cardSavings - cardExpenses);
         }
-    }, [expenses_amount, savings_amount, incomes_amount]);
+    }, [expenses_amount, savings_amount, incomes_amount, cashExpenses, cashSavings, cashIncomes, cardExpenses, cardSavings, cardIncomes]);
 
     const handleExpensesOverviewClick = () => {
-        navigate('/expenses-overview');
+        navigate('/expenses-overview', { state: {cashExpenses, cardExpenses, expenses_amount} });
     };
 
     const handleSavingsOverviewClick = () => {
-        navigate('/savings-overview');
+        navigate('/savings-overview', { state: {cashSavings, cardSavings, savings_amount} });
     };
 
-
+    //TODO: valorar hacer las peticiones desde aquí y pasar los datos
+    const handleAllIncomesViewClick = () => {
+        navigate('/transactions', { state: { transaction_type:"incomes" } });
+    };
 
     return (
         <div>
@@ -79,15 +93,21 @@ export function HomeView() {
 
                 <div className="divider"></div>
 
-                <button onClick={handleExpensesOverviewClick}>Gastos</button>{expenses_amount}
-                <button onClick={handleSavingsOverviewClick}>Ahorros</button>{savings_amount}
-                {/* <button onClick={handleIncomesOverviewClick}>Ingresos</button>{incomes_amount} */}
-                <button>Ingresos</button>{incomes_amount}
-
-            
-                <div className="flex justify-center">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSusawchJOB2j6kQxqZQdFB6BrK5LtquwlLvQ&s" alt="placeholder" />
-                </div>
+                <button style={{ backgroundColor: "gold" }} onClick={handleExpensesOverviewClick}>
+                    <p>Gastos</p>
+                    <p>{expenses_amount} €</p>
+                </button>
+                <button style={{ backgroundColor: "orange" }} onClick={handleSavingsOverviewClick}>
+                    <p>Ahorros</p>
+                    <p>{savings_amount} €</p>
+                </button>
+                <button style={{ backgroundColor: "tomato" }} onClick={handleAllIncomesViewClick}>
+                    <p>Ingresos</p>
+                    <p>{incomes_amount} €</p>
+                </button>
+                <PieChart expenses_amount={expenses_amount} savings_amount={savings_amount} incomes_amount={incomes_amount} />
+                    {/* <PieChart expenses_amount="500" savings_amount="200" incomes_amount="800" /> */}
+                
            
                 <div className="divider"></div>
 
