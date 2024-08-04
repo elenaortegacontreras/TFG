36638f29e82669684aec72318a0630ed01be192b
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-08-04 12:26:23 +0200

    FIx cards resize

--- src/frontend/vite-project/src/components/CategoryBudgetPanel.jsx
+++ src/frontend/vite-project/src/components/CategoryBudgetPanel.jsx
@@ -7,7 +7,7 @@ export function CategoryBudgetPanel({ budgets }) {
         <div className="container mx-auto p-4">
             <div className="flex flex-wrap justify-center">
                 {budgets.map((budget) => (
-                    <div key={budget.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+                    <div key={budget.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
                         <CategoryBudgetCard
                             id={budget.id}
                             name={budget.name} 
