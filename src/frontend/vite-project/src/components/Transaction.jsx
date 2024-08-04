9db6531a50309ea5363db12b94d8e062920ecd23
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-08-04 19:45:52 +0200

    Change button colours

--- src/frontend/vite-project/src/components/Transaction.jsx
+++ src/frontend/vite-project/src/components/Transaction.jsx
@@ -15,8 +15,8 @@ export function Transaction({transaction_id, transaction_type, name, category_na
                 </div>
                 <div className="flex items-center justify-between self-end">
                     <p className="px-4">{formattedDate}</p>
-                    <button className="btn" onClick={()=>document.getElementById(modalId).showModal()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <button className="btn btn-icon" onClick={()=>document.getElementById(modalId).showModal()} style={{backgroundColor: 'transparent'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#4f46e5" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                         </svg>
                     </button>
