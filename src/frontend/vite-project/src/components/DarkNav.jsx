48d8bd92632f10580ce1a6e1c4cdb0f61bd21390
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-08-04 18:12:15 +0200

    Make links work

--- src/frontend/vite-project/src/components/DarkNav.jsx
+++ src/frontend/vite-project/src/components/DarkNav.jsx
@@ -8,12 +8,12 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Home', href: '/', current: true },
+  { name: 'Ingresos', href: '/transactions', current: false, state: { transaction_type: "incomes" } },
+  { name: 'Ahorros', href: '/savings-overview', current: false },
+  { name: 'Gastos', href: '/expenses-overview', current: false },
 ]
+
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
