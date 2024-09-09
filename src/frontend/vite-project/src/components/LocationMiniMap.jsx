f398ea83cdeda55e06759fc22027c88cb26d787a
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-09-09 03:19:51 +0200

    Add location mini map in OCR expense form

--- src/frontend/vite-project/src/components/LocationMiniMap.jsx
+++ src/frontend/vite-project/src/components/LocationMiniMap.jsx
@@ -1,26 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet.locatecontrol';
-// import 'leaflet/dist/leaflet.css';
 
 export function LocationMiniMap() {
     const mapRef = useRef(null);
+    // const [places, setPlaces] = useState([]);
 
     useEffect(() => {
-        if (!mapRef.current) return;
+        if (!mapRef.current) return; // 37.14348360812482, -3.6323339037885454
 
-        const southWest = L.latLng(25.5, -20.0); // Suroeste (Islas Canarias)
-        const northEast = L.latLng(47.0, 15.0);   // Noreste (noreste de España)
-        const bounds = L.latLngBounds(southWest, northEast);
+        // const southWest = L.latLng(25.5, -20.0); // Suroeste (Islas Canarias)
+        // const northEast = L.latLng(47.0, 15.0);   // Noreste (noreste de España)
+        // const bounds = L.latLngBounds(southWest, northEast);
 
-        // Crear el mapa deshabilitando la interacción del usuario
         const map = L.map(mapRef.current, {
             center: [40.4637, -3.7492], // Coordenadas iniciales centradas en España
             zoom: 10,
             minZoom: 10,
             maxZoom: 20,
-            maxBounds: bounds,
-            maxBoundsViscosity: 1.0,
+            // maxBounds: bounds,
+            // maxBoundsViscosity: 1.0,
             // zoomControl: false, // Desactivar controles de zoom
             dragging: false, // Desactivar arrastre del mapa
             scrollWheelZoom: false, // Desactivar zoom con la rueda del ratón
@@ -34,7 +33,7 @@ export function LocationMiniMap() {
             attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
 
-        L.control.locate({
+        const locateControl = L.control.locate({
             position: 'topleft',
             flyTo: true,
             setView: 'untilPanOrZoom',
