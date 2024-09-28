601ba39bdc81b3f425a2cb57a8deeef45f5c15fa
elenaortegacontreras <elena.ortega.grx@gmail.com>
2024-09-28 19:15:23 +0200

    Add incomes view

---
+++ src/frontend/vite-project/src/components/IncomesOverviewView.jsx
@@ -0,0 +1,73 @@
+import { ResumeTitle } from './ResumeTitle.jsx'
+import { TransactionPanel } from './TransactionPanel.jsx'
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export function IncomesOverviewView() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const state = location.state;
+    console.log(state); 
+
+    const [incomes, setIncomes] = useState([]);
+    const [incomes_amount, setIncomesAmount] = useState(null);
+    const [cashIncomes, setCashIncomes] = useState(0);
+    const [cardIncomes, setCardIncomes] = useState(0);
+    const currentMonth = new Date().getMonth() + 1;
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/total_incomes')
+            .then(response => {
+                setIncomesAmount(response.data.amount);
+                setCashIncomes(response.data.cash);
+                setCardIncomes(response.data.card);
+            })
+            .catch(error => {
+                console.error('Error fetching the incomes:', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/incomes')
+            .then(response => {
+                setIncomes(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching the incomes:', error);
+        });
+    }, []);
+
+    const handleAllIncomesViewClick = () => {
+        navigate('/transactions', { state: { transaction_type:"incomes" } });
+    };
+
+    const current_month_incomes = incomes.filter(income => {
+        const incomeDate = new Date(income.insert_date);
+        return incomeDate.getMonth() + 1 === currentMonth;
+    });
+
+    return (
+        <div>
+            <ResumeTitle amount={incomes_amount} title="Ingresos este mes" card={cardIncomes} cash={cashIncomes} currency="€"/>
+
+            <div className="divider"></div>
+
+            <div>
+                <p><strong>Ingresos de este mes</strong></p>
+                <TransactionPanel transactions={current_month_incomes}/>
+            </div> 
+
+            <div className="divider"></div>
+
+            <div className="panel flex w-full justify-evenly">
+                <button onClick={handleAllIncomesViewClick} style={{ background: 'none', border: 'none', cursor: 'pointer', textDecoration: 'underline' }}>Ver todos mis ingresos</button>
+            </div>
+
+            <div className="divider"></div>
+
+
+        </div>
+    );
+}
